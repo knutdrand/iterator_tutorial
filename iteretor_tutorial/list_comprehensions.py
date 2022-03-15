0aad5214a385a8a43389597611de04e50cca4c2d
Knut Rand <knutdrand@gmail.com>
2022-03-15 12:55:46 +0100

    init

---
+++ iteretor_tutorial/list_comprehensions.py
@@ -0,0 +1,51 @@
+# @pytest.fixture
+# def sequences():
+#    return ["agctctt", "tggtgta", "gcttagt", "aaaagtctgt"]
+
+
+def simple_loop(sequences):
+    lengths = []
+    for sequence in sequences:
+        lengths.append(len(sequence))
+    return lengths
+
+def double_loop(sequences):
+    all_ascii_codes = []
+    for sequence in sequences:
+        ascii_codes = []
+        for char in sequence:
+            ascii_codes.append(ord(char))
+        all_ascii_codes.append(ascii_codes)
+    return all_ascii_codes
+
+def set_comprehension(sequences):
+    alphabet = set([])
+    for sequence in sequences:
+        for char in sequence:
+            alphabet.add(char)
+
+    return alphabet
+
+def two_operations(sequences):
+    lens = []
+    for sequence in  sequences:
+        adjusted_sequence = sequence.replace("a", "")
+        lens.append(len(adjusted_sequence))
+    return lens
+                    
+def len_sum(sequences):
+    total = 0
+    for sequence in sequences:
+        total += len(sequence)
+
+    return total
+
+def age_filter(sequences, ages):
+    assert len(sequences) == len(ages)
+    a_counts = []
+    for i in range(len(sequences)):
+        if ages[i]>=15:
+            a_counts.append(sequences[i].count("a"))
+    return a_counts
+    
+
---
+++ tests/test_iteretor_tutorial.py
@@ -0,0 +1,32 @@
+#!/usr/bin/env python
+
+"""Tests for `iteretor_tutorial` package."""
+
+import pytest
+from iteretor_tutorial.list_comprehensions import *
+
+@pytest.fixture
+def sequences():
+    return ["agctctt", "tggtgta", "gcttagt", "aaaagtctgt", "cccta"]
+
+@pytest.fixture
+def ages():
+    return [10, 15, 17, 19, 12]
+
+def test_simple(sequences):
+    assert simple_loop(sequences) == [len(s) for s in sequences]
+
+def test_double(sequences):
+    assert double_loop(sequences) == [[ord(c) for c in s] for s in sequences]
+
+def test_setcomprehension(sequences):
+    assert set_comprehension(sequences) == {c for sequence in sequences for c in sequence}
+
+def test_double(sequences):
+    assert two_operations(sequences) == [len(s.replace("a", "")) for s in sequences]
+
+def test_sum(sequences):
+    assert len_sum(sequences) == sum(len(s) for s in sequences)
+
+def test_zip(sequences, ages):
+    assert age_filter(sequences, ages) == [s.count("a") for s, a in zip(sequences, ages) if a>=15]
